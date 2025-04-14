c85a530a718c53b8b48143ee5f419336cceca3ad
Hasnahana Chetia <drhasnachetia@gmail.com>
2025-04-14 16:37:51 -0400

    Update Untitled.R
    testing pull

--- Untitled.R
+++ Untitled.R
@@ -1,1 +1,4 @@
 library(viz)
+#hey! testing Pull now. 
+Pull is for when the Github repo was updated online, but your local Rstudio is needs to be synced with the online version.
+Push is when I update code locally, but want to update it on Github repo.
